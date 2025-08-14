d1e1a0926b9471b3082b87633d9bd9e2e41ba4fd
Prajwal2246 <janbandhuprajwal28@gmail.com>
2025-08-14 21:59:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 //your code here!
+document.addEventListener("DOMContentLoaded",()=>{
+	const list = document.getElementById("infi-list");
+	let counter=1;
+
+
+	function addListItems(count){
+		for(let i=0;i<count;i++){
+			const li = document.createElement("li");
+			li.textContent = `List Item ${counter++}`;
+			list.appendChild(li);
+		}
+	}
+	addListItems(10);
+
+	window.addEventListener("scroll",()=>{
+		if(window.innerHeight + window.scrollY >= document.body.offsetHeight){
+			addListItems(2);
+		}
+	})
+})
 
